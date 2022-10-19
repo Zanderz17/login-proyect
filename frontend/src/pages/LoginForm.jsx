5dcf4a6efa804e48268894d2bc8b6853fcb5c5f1
Brayan Eduardo Gomero Castillo <brayangc@MacBook-Air-de-Brayan.local>
2022-10-19 05:21:32 -0500

    client-side and server-side connected

--- frontend/src/pages/LoginForm.jsx
+++ frontend/src/pages/LoginForm.jsx
@@ -1,30 +1,47 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import '../css/pages-styles/LoginForm.css';
+import Axios from 'axios';
 
 function LoginForm() {
+    const [loginCorreo, setLoginCorreo] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const login = () => {
+        Axios({
+            method: 'POST',
+            data: {
+                correo: loginCorreo,
+                password: loginPassword
+            },
+            withCredentials: true,
+            url: "http://localhost:9000/users/login"
+        });
+    };
+
     return (
         <div className="principal-login">
             <div className="login">
                 <img src={require('../img/logo.jpg')} alt='logoimg'/>
                 <h1 className="text-center">Iniciar sesión</h1>   
-                <form className="needs-validation">
+                <form className="needs-validation" onSubmit={login}>
                     <div className="form-group">
                         <label className="form-label" htmlFor="email">Correo electrónico</label>
-                        <input className="form-control" type="email" id="email" placeholder="sanders@hotmail.com" required />
+                        <input className="form-control" type="email" id="email" placeholder="sanders@hotmail.com" onChange={(e) => setLoginCorreo(e.target.value)} required />
                     </div>
+
                     <div className="form-group">
                         <label className="form-label" htmlFor="password">Contraseña</label>
-                        <input className="form-control" type="password" id="password" placeholder="********************" required />
+                        <input className="form-control" type="password" id="password" placeholder="********************" onChange={(e) => setLoginPassword(e.target.value)} required />
                         {/* 
                             <div className="invalid-feedback">
                                 Su correo electrónico o contraseña no coinciden
                             </div>
                         */}
-                    </div>    
+                    </div>
+
                     <input className="btn btn-success w-100" type="submit" value="Iniciar Sesión" />
+
                     <div className="form-check text-center">
-                        <label className="form-check-label">¿No tienes cuenta? <a href='#'>Registrate</a></label>
+                        <label className="form-check-label">¿No tienes cuenta? <a href='/users/signup'>Registrate</a></label>
                     </div>
                 </form>
             </div>
--- frontend/src/pages/RegisterForm.jsx
+++ frontend/src/pages/RegisterForm.jsx
@@ -1,52 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom"
+import Axios from 'axios';
 import '../css/pages-styles/RegisterForm.css';
 
 function RegisterForm() {
+    const [registerCorreo, setRegisterCorreo] = useState("");
+    const [registerNombre, setRegisterNombre] = useState("");
+    const [registerApellidos, setRegisterApellidos] = useState("");
+    const [registerRol, setRegisterRol] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const navigate = useNavigate();
+    const register = (e) => {
+        Axios({
+            method: 'POST',
+            data: {
+                correo: registerCorreo,
+                nombre: registerNombre,
+                apellidos: registerApellidos,
+                rol: registerRol,
+                password: registerPassword
+            },
+            withCredentials: true,
+            url: "http://localhost:9000/users/register"
+        });
+        e.preventDefault();
+        //navigate('/signin');
+    };
+
     return (
         <div className="principal-register-form">
             <div className="register-form">
                 <img src={require('../img/logo.jpg')} alt='logoimg'/>
                 <h1 className="text-center">Verifique que los datos mostrados sean correctos</h1>   
-                <form className="needs-validation">
+                <form className="needs-validation" onSubmit={register}>
                     <div className="form-group">
                         <label className="form-label" htmlFor="email">Correo electrónico</label>
-                        <input className="form-control" type="email" id="email" placeholder="sanderschancan@hotmail.com" required />
+                        <input className="form-control" type="email" id="email" placeholder="sanderschancan@hotmail.com" onChange={e => setRegisterCorreo(e.target.value)} required />
                     </div>
 
                     <div className="form-group">
                         <label className="form-label" htmlFor="name">Nombre</label>
-                        <input className="form-control" type="text" id="name" placeholder="Sanders" required />
+                        <input className="form-control" type="text" id="name" placeholder="Sanders" onChange={e => setRegisterNombre(e.target.value)} required />
                     </div>  
 
                     <div className="form-group">
                         <label className="form-label" htmlFor="lastName">Apellidos</label>
-                        <input className="form-control" type="text" id="lastName" placeholder="Chancan Chanca" required />
+                        <input className="form-control" type="text" id="lastName" placeholder="Chancan Chanca" onChange={e => setRegisterApellidos(e.target.value)} required />
                     </div>
 
                     <div className="form-group">
-                        <label className="form-label" htmlFor="role">Apellidos</label>
-                        <input className="form-control" type="text" id="role" placeholder="Jefe de Compras" required />
+                        <label className="form-label" htmlFor="role">Rol</label>
+                        <input className="form-control" type="text" id="role" placeholder="Jefe de Compras" onChange={e => setRegisterRol(e.target.value)} required />
                     </div>
 
                     <div className="form-group">
                         <label className="form-label" htmlFor="password">Contraseña</label>
-                        <input className="form-control" type="password" id="password" placeholder="********************" required />
+                        <input className="form-control" type="password" id="password" placeholder="********************" onChange={e => setRegisterPassword(e.target.value)} required />
                     </div> 
-
-                    <div className="form-group">
-                        <label className="form-label" htmlFor="passwordConfirm">Confirmar contraseña</label>
-                        <input className="form-control" type="password" id="passwordConfirm" placeholder="********************" required />
-                    </div> 
-
-                    <div className="form-group form-check">
-                            <input className="form-check-input" type="checkbox" id="check" />
-                            <label className="form-check-label"> Acepto <a href='#'>los términos y condiciones de uso</a></label>
-                    </div>
 
                     <input className="btn btn-success w-100" type="submit" value="Registrar" />
                     
                     <div className="form-group text-center">
-                        <label className="form-check-label">¿Ya tienes cuenta? <a href='#'>Conéctate</a></label>
+                        <label className="form-check-label">¿Ya tienes cuenta? <a href='/users/signin'>Conéctate</a></label>
                     </div>
                 </form>
             </div>
